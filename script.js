6387782032a5f2b06b74b197e2b9eab96a3f1944
savvyinfo <janghkoh@yahoo.com>
2017-05-03 13:05:44 -0700

    script js updated - JOhn Ko

--- script.js
+++ script.js
@@ -1,125 +1,129 @@
 var test_array =
         [
-        [0, 0, 0, 0, 0, 1],
-        [0, 0, 1, 0, 1, 0],
-        [0, 0, 0, 1, 0, 0],
-        [0, 0, 1, 0, 1, 0],
-        [0, 0, 0, 0, 0, 1]
+        [1, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0]
         ];
 
-var temp_win_count = 1;
+var temp_win_count = 0;
 var class_player1 = 1;  // check player1
 
 function check_for_win(row, col){
-    // row : to right end
+
+    // row : to right
     for(var c = col; c < test_array[row].length; c++){
-        if(test_array[row][c+1] === class_player1) {
+        if(test_array[row][c] === class_player1) {
             temp_win_count++;
             if (temp_win_count === 4) {
-                console.log('You won the game!');
-                break;
+                win_check = true;
+                console.log('to right: You won the game!');
+                return;
             }
         } else {
-            console.log('to right: you did not match 4...yet');
+            console.log('to right: not match 4');
+            break;
         }
     };
-    // row : to left end
-    for(var c = col; c >= 0; c--){
-        if(test_array[row][c-1] === class_player1) {
+
+    // row : to left
+    for(var c = col -1; c >= 0; c--){
+        if(test_array[row][c] === class_player1) {
             temp_win_count++;
             if (temp_win_count === 4) {
-                console.log('You won the game!');
-                break;
+                console.log('to left: You won the game!');
+                return;
             }
         }  else {
-                console.log('to left: you did not match 4');
+            console.log('to left: not match 4');
+            break;
         }
     };
-    temp_win_count = 1;
+    temp_win_count = 0;
 
     // col : upward
     for(var r = row; r >= 0; r--){
         if(test_array[r][col] === class_player1){
             temp_win_count++;
             if(temp_win_count === 4){
-                console.log('You won the game!');
-                break;
+                console.log('upward: You won the game!');
+                return;
             }
         } else {
-            console.log('upward: you did not match 4');
+            console.log('upward: not match 4');
+            break;
         }
     };
+
     // col : downward
-    for(var r = row; r < test_array.length; r++){
+    for(var r = row +1; r < test_array.length; r++){
         if(test_array[r][col] === class_player1){
             temp_win_count++;
             if(temp_win_count === 4){
-                console.log('You won the game!');
-                break;
+                console.log('downward: You won the game!');
+                return;
             }
         } else {
-            console.log('downward: you did not match 4');
+            console.log('downward: not match 4');
+            break;
         }
     };
-    temp_win_count = 1;
+    temp_win_count = 0;
 
-    // diagonal : right - up
+    // diagonal : right-up
     for(var r = row, c = col; r > 0 && c < test_array[row].length; r--, c++){
-            if(test_array[r-1][c+1] === class_player1) {
+            if(test_array[r][c] === class_player1) {
                 temp_win_count++;
                 if (temp_win_count === 4) {
-                    console.log('You won the game!');
-                    break;
+                    console.log('right-up : You won the game!');
+                    return;
                 }
             } else {
-                console.log('right - up : you did not match 4...yet');
-            }
-    };
-    // diagonal : left - down
-    for(var r = row, c = col; r < test_array.length && c > 0; r++, c--){
-        if(test_array[r+1][c-1] === class_player1) {
-            temp_win_count++;
-            if (temp_win_count === 4) {
-                console.log('You won the game!');
+                console.log('right-up : not match 4');
                 break;
             }
+    };
+    // diagonal : left-down
+    for(var r = row +1, c = col-1; r < test_array.length && c > 0; r++, c--){
+        if(test_array[r][c] === class_player1) {
+            temp_win_count++;
+            if (temp_win_count === 4) {
+                console.log('left-down : You won the game!');
+                return;
+            }
         } else {
-            console.log('left - down : you did not match 4...yet');
+            console.log('left-down : not match 4');
+            break;
         }
     };
-    temp_win_count = 1;
+    temp_win_count = 0;
 
-    // diagonal : right - down
-    for(var r = row, c = col; r < test_array[row].length && c < test_array[row].length; r++, c++){
-        if(test_array[r+1][c+1] === class_player1) {
+    // diagonal : right-down
+    for(var r = row, c = col; r < test_array.length && c < test_array[row].length; r++, c++){
+        if(test_array[r][c] === class_player1) {
             temp_win_count++;
             if (temp_win_count === 4) {
-                console.log('You won the game!');
-                break;
+                console.log('right-down : You won the game!');
+                return;
             }
         } else {
-            console.log('right - down : you did not match 4...yet');
+            console.log('right-down : not match 4');
+            break;
         }
     };
 
-    //diagonal : left - up
-    for(var r = row, c = col; r > 0 && c > 0; r--, c--){
-        if(test_array[r-1][c-1] === class_player1) {
+    //diagonal : left-up
+    for(var r = row-1, c = col-1; r > 0 && c > 0; r--, c--){
+        if(test_array[r][c] === class_player1) {
             temp_win_count++;
             if (temp_win_count === 4) {
-                console.log('You won the game!');
-                break;
+                console.log('left-up : You won the game!');
+                return;
             }
         } else {
-            console.log('left - up : you did not match 4...yet');
+            console.log('left-up : not match 4');
+            break;
         }
     };
 };
-
-
-
-
-
-
-
-
